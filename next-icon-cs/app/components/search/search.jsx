aeb01dffc458f90dabd2f4210923b88ab505ddf1
Alex <a.perekrestov@gmail.com>
2024-05-08 14:55:18 +0300

    Работа над воформлением выпадающего списка формы поиска

--- next-icon-cs/app/components/search/search.jsx
+++ next-icon-cs/app/components/search/search.jsx
@@ -1,24 +1,32 @@
 'use client'
-
+import { useRef } from 'react'
 import styles from './search.module.scss'
 
-export default function Search() {
+export default function Search({ searchText = '' }) {
+	const userQuery = useRef(null)
+
 	return (
 		<form className={styles.searchForm} autoComplete='off'>
 			<input
+				autoComplete='off'
 				type='search'
 				name='search'
 				placeholder='Поиск графических иконок'
-				defaultValue='{searchText}'
+				defaultValue={searchText}
 				className={styles.searchForm__input}
-				// ref={userQuery}
-				// onKeyDown={searchKeyDown}
+				ref={userQuery}
+			// onKeyDown={searchKeyDown}
 			/>
-			<input
-				type='submit'
-				value=""
-				className={styles.searchForm__btn}
-			/>
+			<button className={styles.searchForm__btn} type='submit'></button>
+
+
+			<select className={styles.searchOptions} id="icons" name="icons" size="5">
+				<option value="volvo">Volvo</option>
+				<option value="saab">Saab</option>
+				<option value="fiat">Fiat</option>
+				<option value="audi">Audi</option>
+			</select>
+
 		</form>
 	)
 }
