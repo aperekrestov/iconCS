5431d426ae0bd53c5e257aa0b12f7f6503191fbc
Alex <a.perekrestov@yandex.ru>
2024-06-11 16:33:34 +0300

    Работа над баннером

--- next-icon-cs/app/components/main/main.jsx
+++ next-icon-cs/app/components/main/main.jsx
@@ -5,9 +5,27 @@ export default function Main() {
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.container + ' ' + 'content_width_large'}>
-				<h1>Визуальный язык позволяет общаться независимо от того, кто вы и где вы</h1>
-				<div className={styles.container__search}>
+				<h1 className='margin_bottom_xl'>Визуальный язык <b>CS</b>oft</h1>
+				<div className={styles.container__search + ' ' + 'margin_bottom_xxxl'}>
 					<SearchBar />
+				</div>
+				<div className={styles.banner}>
+					<div className={styles.banner__line + ' ' + styles.pd_right + ' ' + 'margin_bottom_xxxl'}>
+						<div className={styles.banner__icon + ' ' + styles.icon1}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon2}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon3}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon4}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon5}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon6}></div>
+					</div>
+					<div className={styles.banner__line + ' ' + styles.pd_left}>
+						<div className={styles.banner__icon + ' ' + styles.icon7}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon8}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon9}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon10}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon11}></div>
+						<div className={styles.banner__icon + ' ' + styles.icon12}></div>
+					</div>
 				</div>
 			</div>
 		</section>
